e772ffa1a079762f710553e026647baeb8656793
Nikolay Anikin <n.anikin@2gis.ru>
2026-01-22 17:25:53 +0300

    fixes

--- src/db.rs
+++ src/db.rs
@@ -2,28 +2,30 @@ use rocksdb::{DB, Options, ColumnFamilyDescriptor, IteratorMode};
 use crate::utils::to_hex;
 use std::error::Error;
 
+/// Структура для работы с RocksDB
 pub struct RockPeekDB {
     db: DB,
 }
 
 impl RockPeekDB {
-    /// Открываем базу с перечислением всех column family (read-only)
+    /// Read-only открытие базы с поддержкой всех CF
     pub fn open(path: &str) -> Result<Self, Box<dyn Error>> {
-        // Получаем список CF из базы
+        // Получаем список всех CF в базе
         let cfs = DB::list_cf(&Options::default(), path)
             .unwrap_or_else(|_| vec!["default".to_string()]);
 
+        // Создаём дескрипторы CF
         let cf_descriptors: Vec<ColumnFamilyDescriptor> = cfs
             .iter()
             .map(|name| ColumnFamilyDescriptor::new(name, Options::default()))
             .collect();
 
-        // Открываем базу с CF дескрипторами
-        let db = DB::open_cf_descriptors(&Options::default(), path, cf_descriptors)?;
-
+        // Открываем базу read-only с этими CF
+        let db = DB::open_cf_descriptors_read_only(&Options::default(), path, cf_descriptors, false)?;
         Ok(Self { db })
     }
 
+    /// Сканируем указанную column family
     pub fn scan(&self, cf_name: &str) -> Result<(), Box<dyn Error>> {
         let cf_handle = self.db.cf_handle(cf_name)
             .ok_or(format!("Column family '{}' not found", cf_name))?;
